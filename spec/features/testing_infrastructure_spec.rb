eae9a4459d58ed11f2f130aa755b5be8cbf3cc11
Aiden Gleave <aidengleave@gmail.com>
2019-01-24 14:45:04 +0000

    Added vs. functionality to root page - passing the test

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+feature 'Start a fight by entering names' do
+  scenario 'inputted name appears on the page' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in 'player_1', with: 'Tim'
+    fill_in 'player_2', with: 'John'
+    click_button 'Submit'
+    expect(page).to have_content "Tim vs. John"
   end
 end
