36aaee18b222ce03fe17266ab64473212e23a3ce
Aiden Gleave <aidengleave@gmail.com>
2019-01-24 15:56:02 +0000

    Created web_helpers and refactor to DRY

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player_1, with: 'Tim'
-    fill_in :player_2, with: 'John'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John: 60HP'
   end
 end
